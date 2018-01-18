8fbc356524e00a464df5f98c07b2be54be9e4555
TaiTD <taitd@rikkeisoft.com>
2018-01-18 16:55:34 +0700

    Lesson 10

---
+++ wp-content/themes/taicristiano/archive.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+<div class="content">
+	<section id="main-content">
+        <div class="archive-title">
+            <h2>
+                <?php
+                    if ( is_tag() ) : printf( __('Posts Tagged: %1$s','thachpham'), single_tag_title( '', false ) );
+                    elseif ( is_category() ) : printf( __('Posts Categorized: %1$s','thachpham'), single_cat_title( '', false ) );
+                    elseif ( is_day() ) : printf( __('Daily Archives: %1$s','thachpham'), get_the_time('l, F j, Y') );
+                    elseif ( is_month() ) : printf( __('Monthly Archives: %1$s','thachpham'), get_the_time('F Y') );
+                    elseif ( is_year() ) : printf( __('Yearly Archives: %1$s','thachpham'), get_the_time('Y') );
+                    endif;
+                ?>
+            </h2>
+        </div>
+        <?php if ( is_tag() || is_category() ) : ?>
+            <div class="archive-description">
+                <?php echo term_description(); ?>
+            </div>
+        <?php endif; ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
+        <?php endwhile; ?>
+        <?php taicristano_pagination(); ?>
+        <?php else : ?>
+        	<?php get_template_part( 'content', 'none' ); ?>
+        <?php endif; ?>
+    </section>
+	<section id="sidebar"></section>
+</div>
+
+<?php get_footer(); ?>
